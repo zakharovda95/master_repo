5384f87ee0713b9f9574c854470fde3b75f1fc48
zakharovda95 <zakharovda95@gmail.com>
2024-04-02 19:27:05 +0300

    Initial commit

---
+++ checkout-submodules.sh
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+
+declare -A branches=(
+    ["HelperClassLib"]="master"
+    ["BeautySaloonTgBot"]="master"
+    # Добавьте сюда другие подмодули и ветки при необходимости
+)
+
+for submodule in $(git submodule status --recursive | awk '{print $2}'); do
+
+    submodule_name=$(basename "$submodule")
+
+    if [ -n "${branches[$submodule_name]}" ]; then
+        branch=${branches[$submodule_name]}
+
+        cd "$submodule" || exit
+
+        echo "Entering '$submodule_name'"
+        git checkout "$branch" && git pull origin "$branch"
+
+        cd - || exit
+    fi
+done
+
+read -p "Нажмите Enter для выхода"
